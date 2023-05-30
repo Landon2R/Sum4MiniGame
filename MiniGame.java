03391091c85f8f8f6a900e6e1f178a8065b8bc6e
Landon2R <125473648+Landon2R@users.noreply.github.com>
2023-05-30 08:50:10 -0400

    Update MiniGame.java

--- MiniGame.java
+++ MiniGame.java
@@ -18,6 +18,10 @@ public class MiniGame {
             util.prt("\033[H\033[2J");
             System.out.println("1,2,3,4,5,6,7");
             display(map, tiles);
+            over = win(map, move);
+            if (over) {
+                break;
+            }
             int col = Integer.parseInt(in.nextLine()) - 1;
             if (canMove(col)) {
 
@@ -62,15 +66,33 @@ public class MiniGame {
         return false;
     }
 
-    public static boolean win(int[][] map,int move) {
-        int colW =0;
-        int rowW =0;
-        int diagRW =0;
-        int diagLW =0;
-        for (int i = 0; i < map.length; i++) {
+    public static boolean win(int[][] map, int move) {
+        int colW = 0;// colum win
+        int rowW = 0;// row win
+        int diagRW = 0;// diaganal right win
+        int diagLW = 0;// diaganal left win
+        for (int i = 0; i < map.length-1; i++) {
             for (int j = 0; j < map[i].length; j++) {
-                
+                for (int w = 0; w < 4; w++) {
+                    if (map[i][j] == move && map[i + 1][j] == move && i + 1 != 6) {
+                        colW++;
+                    }
+                    if (map[i][j] == move && map[i][j + 1] == move && j + 1 != 7) {
+                        rowW++;
+                    }
+                    if (map[i][j] == move && map[i + 1][j + 1] == move && i + 1 != 6 && j + 1 != 7) {
+                        diagRW++;
+                    }
+                    if (map[i][j] == move && map[i - 1][j - 1] == move && i - 1 != -1 && j - 1 != -1) {
+                        diagLW++;
+                    }
+                }
             }
+        }
+        if (colW >= 4 || rowW >= 4 || diagRW >= 4 || diagLW >= 4) {
+            return true;
+        } else {
+            return false;
         }
     }
 
