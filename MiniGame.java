c6affacab90a9a1f94d25077499c2081557cc32e
Landon2R <125473648+Landon2R@users.noreply.github.com>
2023-05-30 09:36:44 -0400

    Update MiniGame.java

--- MiniGame.java
+++ MiniGame.java
@@ -11,14 +11,15 @@ public class MiniGame {
                 { 0, 0, 0, 0, 0, 0, 0 },
         };
         boolean over = false;
-        String[] tiles = { "O ", "\033[0;31m# \033[0m", "\033[0;34m@ \033[0m" };
+        String[] tiles = { "O ", "\033[0;31m@ \033[0m", "\033[0;34m@ \033[0m" };
         int move = 1;
         Scanner in = new Scanner(System.in);
         while (!over) {
             util.prt("\033[H\033[2J");
-            System.out.println("1,2,3,4,5,6,7");
+            util.prt("1,2,3,4,5,6,7");
+        
             display(map, tiles);
-            over = win(map, move);
+
             if (over) {
                 break;
             }
@@ -34,12 +35,16 @@ public class MiniGame {
                         break;
                     }
                 }
-                if (move == 1) {
-                    move = 2;
-                } else {
-                    move = 1;
-                }
-
+            }
+            display(map, tiles);
+            over = win(map, move);
+            if (over) {
+                util.prt("game over");
+            }
+            if (move == 1) {
+                move = 2;
+            } else {
+                move = 1;
             }
 
         }
@@ -60,40 +65,40 @@ public class MiniGame {
 
     public static boolean canMove(int col) {
         // Check whether within bounds.
-        if (col >= 0 && col < 8)
+        if (col >= 0 && col < 7)
             return true;
 
         return false;
     }
 
     public static boolean win(int[][] map, int move) {
-        int colW = 0;// colum win
-        int rowW = 0;// row win
-        int diagRW = 0;// diaganal right win
-        int diagLW = 0;// diaganal left win
-        for (int i = 0; i < map.length-1; i++) {
+
+        for (int i = 0; i < map.length - 1; i++) {
             for (int j = 0; j < map[i].length; j++) {
-                for (int w = 0; w < 4; w++) {
-                    if (map[i][j] == move && map[i + 1][j] == move && i + 1 != 6) {
+                int colW = 0;// colum win
+                int rowW = 0;// row win
+                int diagRW = 0;// diaganal right win
+                int diagLW = 0;// diaganal left win
+                for (int w = 0; w < 4; w++) {// count how many of a players characters are in a row
+                    if (i + w < 6 && map[i][j] == move && map[i + w][j] == move) {
                         colW++;
                     }
-                    if (map[i][j] == move && map[i][j + 1] == move && j + 1 != 7) {
+                    if (j + w < 7 && map[i][j] == move && map[i][j + w] == move) {
                         rowW++;
                     }
-                    if (map[i][j] == move && map[i + 1][j + 1] == move && i + 1 != 6 && j + 1 != 7) {
+                    if (j + w < 7 && i + w < 6 && map[i][j] == move && map[i + w][j + w] == move) {
                         diagRW++;
                     }
-                    if (map[i][j] == move && map[i - 1][j - 1] == move && i - 1 != -1 && j - 1 != -1) {
+                    if (i - w > -1 && j - w > -1 && map[i][j] == move && map[i - w][j - w] == move) {
                         diagLW++;
                     }
                 }
+                if (colW >= 4 || rowW >= 4 || diagRW >= 4 || diagLW >= 4) {
+                    return true;
+                }
             }
         }
-        if (colW >= 4 || rowW >= 4 || diagRW >= 4 || diagLW >= 4) {
-            return true;
-        } else {
-            return false;
-        }
+        return false;
     }
 
 }
